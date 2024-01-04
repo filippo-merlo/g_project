c7a143f53c41eb2498a1accda62d5dc951901c03
Filippo <filippo99merlo@gmail.com>
2024-01-04 10:40:01 +0100

    ptova

--- code/config.py
+++ code/config.py
@@ -108,6 +108,7 @@ for v in dic_train_logical.values():
 		if n not in others:
 			all_vocabs.append(n)
 
+ygdsuy = 0
 #print(all_vocabs)
 #pprint(dic_train_logical)
 #print(types_logical_with_learning)

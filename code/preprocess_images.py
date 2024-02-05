bfdb9537265227b55bf438abfe9ee65b65be54ca
Filippo <filippo99merlo@gmail.com>
2024-02-05 12:17:49 +0100

    pp

--- code/preprocess_images.py
+++ code/preprocess_images.py
@@ -15,6 +15,8 @@ def get_preprocessed_images(in_path,out_path):
     images_sets = [
         'train',
         'test',
+        'novel_train',
+        'novel_test'
     ]
     clip_model, clip_preprocessor = clip.load("ViT-B/32", device=device)
     clip_model.eval()
@@ -24,13 +26,13 @@ def get_preprocessed_images(in_path,out_path):
         folder_path = os.path.join(in_path, images_set)
         for filename in os.listdir(folder_path):
             file_path = os.path.join(folder_path, filename)
-            image = clip_preprocessor(Image.open(file_path))
-            out_file_path = os.path.join(out_path, images_set, filename)
-            out_file_path = re.sub(r'\.png$', '', out_file_path, flags=re.IGNORECASE)
-            with open(out_file_path+'.pickle', 'wb') as file:
-                pickle.dump(image, file)
+            if re.search(re.escape(r'\.png$'),file_path):
+                image = clip_preprocessor(Image.open(file_path))
+                out_file_path = os.path.join(out_path, images_set, filename)
+                out_file_path = re.sub(r'\.png$', '', out_file_path, flags=re.IGNORECASE)
+                with open(out_file_path+'.pickle', 'wb') as file:
+                    pickle.dump(image, file)
 
-        
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
 

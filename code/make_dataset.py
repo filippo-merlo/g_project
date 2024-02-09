a358ab7396190ab565fcdcd4e3c56d2f744b7ade
Filippo <filippo99merlo@gmail.com>
2024-02-09 13:11:21 +0100

    Add save_list function call to make_dataset.py

--- code/make_dataset.py
+++ code/make_dataset.py
@@ -22,6 +22,14 @@ def save_list(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file)
 
+def get_key_from_value(dictionary, target_value):
+    for key, value in dictionary.items():
+        if value == target_value:
+            return key
+    # If the target_value is not found in the dictionary, you can return a default value or raise an exception.
+    # In this example, None is returned if the value is not found.
+    return None
+
 # Build the dataset object
 def get_datasets(in_path,out_path):
     parameters_list = [
@@ -36,32 +44,30 @@ def get_datasets(in_path,out_path):
     for parameters in parameters_list:
         source, in_base, types = parameters
         out_path = os.path.join(out_path, parameters[0]+'_dataset.json')
+        save_list(out_path, []) ## After doing this one time, comment this line
         dt = MyDataset(in_path, source, in_base, types, dic, vocab,
                             clip_preprocessor=clip_preprocessor)
         
-        for attribute, lessons in dic_train_logical.items():
-            l = list(dic_train_logical.keys())
-            print('Attributes completed:',l.index(attribute)/len(dic_train_logical.items()),'%')
+        for lesson in vocab:
+            attribute = get_key_from_value(dic, lesson)
 
-            for lesson in lessons:
-                 
-                 for i in range(500):
-                    print('Batches completed:',i/500,'%')
-                    if source == 'train':
-                        train = True
-                    else:
-                        train = False
-                    base_names_sim, base_names_dif = dt.get_paired_batches_names(attribute, lesson, 132, train)
-                    all_lessons = load_list(out_path)
-                    all_lessons.append(
-                        {
-                        'attribute' : attribute,
-                        'lesson' : lesson,
-                        'base_names_sim' : base_names_sim,
-                        'base_names_sim' : base_names_dif
-                        }
-                    )
-                    save_list(out_path, all_lessons)
+            for i in range(500):
+            print('Batches completed:',i/500,'%')
+            if source == 'train':
+                train = True
+            else:
+                train = False
+            base_names_sim, base_names_dif = dt.get_paired_batches_names(attribute, lesson, 132, train)
+            all_lessons = load_list(out_path)
+            all_lessons.append(
+                {
+                'attribute' : attribute,
+                'lesson' : lesson,
+                'base_names_sim' : base_names_sim,
+                'base_names_sim' : base_names_dif
+                }
+            )
+            save_list(out_path, all_lessons)
 
 
 if __name__ == '__main__':
